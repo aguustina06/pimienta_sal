ac38e8ebd3049c0b96aae4a195c522d613f45b9b
aguustina06 <agustina_0610@hotmail.com>
2021-09-14 18:37:32 -0300

    pimienta y sal

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+
+const {index, detalle} = require('../controllers/indexController')
+
+/* GET home page. */
+router.get('/', index);
+
+router.get('/detalle/:id', detalle)
+
+module.exports = router;
